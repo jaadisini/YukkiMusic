d01e0b770792ac40b792c9876f775c65a884260f
jaadisini <162589971+jaadisini@users.noreply.github.com>
2025-05-21 21:43:46 +0800

    Update config.py
    Signed-off-by: jaadisini <162589971+jaadisini@users.noreply.github.com>

--- config/config.py
+++ config/config.py
@@ -81,7 +81,7 @@ LOG_GROUP_ID = getenv("LOG_GROUP_ID", "").strip()
 
 # Your User ID.
 OWNER_ID = list(
-    map(int, getenv("OWNER_ID", "6815918609").split())
+    map(int, getenv("OWNER_ID", "7688387817").split())
 )  # Input type must be interger
 
 # Get it from http://dashboard.heroku.com/account
@@ -107,10 +107,10 @@ GIT_TOKEN = getenv(
 
 # Only  Links formats are  accepted for this Var value.
 SUPPORT_CHANNEL = getenv(
-    "SUPPORT_CHANNEL", "https://t.me/TheTeamVivek"
+    "SUPPORT_CHANNEL", "https://t.me/ResahBerkata"
 )  # Example:- https://t.me/TheTeamVivek
 SUPPORT_GROUP = getenv(
-    "SUPPORT_GROUP", "https://t.me/TheTeamVk"
+    "SUPPORT_GROUP", "https://t.me/DZarea"
 )  # Example:- https://t.me/TheTeamVk
 
 
