4fda20699f962589283006245bec59c6ddbadeb0
jaadisini <162589971+jaadisini@users.noreply.github.com>
2025-05-21 22:07:48 +0800

    Update start.py
    Signed-off-by: jaadisini <162589971+jaadisini@users.noreply.github.com>

--- YukkiMusic/utils/inline/start.py
+++ YukkiMusic/utils/inline/start.py
@@ -74,15 +74,14 @@ def private_panel(_, BOT_USERNAME, OWNER: bool | int = None):
     if GITHUB_REPO and OWNER:
         buttons.append(
             [
-                InlineKeyboardButton(text=_["S_B_7"], user_id=OWNER),
-                InlineKeyboardButton(text=_["S_B_6"], url=f"{GITHUB_REPO}"),
+                InlineKeyboardButton(text=_["S_B_7"], user_id=OWNER)),
             ]
         )
     else:
         if GITHUB_REPO:
             buttons.append(
                 [
-                    InlineKeyboardButton(text=_["S_B_6"], url=f"{GITHUB_REPO}"),
+                    InlineKeyboardButton(text=_["S_B_6"], url=f"xnxx.com"),
                 ]
             )
 
