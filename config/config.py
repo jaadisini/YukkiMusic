339973cf30e7114f930ec00b18e958da11893155
jaadisini <162589971+jaadisini@users.noreply.github.com>
2025-05-22 14:44:35 +0800

    Update config.py
    Signed-off-by: jaadisini <162589971+jaadisini@users.noreply.github.com>

--- config/config.py
+++ config/config.py
@@ -94,7 +94,7 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 # For customized or modified Repository
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/TheTeamVivek/YukkiMusic",
+    "https://github.com/jaadisini/YukkiMusic",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 
